a7ae232f34b5b3ea8032dda5cde8c01b97412871
aaronFortuno <aaron.fortuno@protonmail.com>
2023-11-05 13:19:40 +0100

    finishing viewmodel

--- app/src/main/java/net/estemon/studio/latorre/ui/LaTorreViewModel.kt
+++ app/src/main/java/net/estemon/studio/latorre/ui/LaTorreViewModel.kt
@@ -3,6 +3,7 @@ package net.estemon.studio.latorre.ui
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import net.estemon.studio.latorre.data.PlaceCategory
 import net.estemon.studio.latorre.data.local.LocalLaTorreDataProvider
 import net.estemon.studio.latorre.model.Place
@@ -27,4 +28,32 @@ class LaTorreViewModel : ViewModel() {
                     ?: LocalLaTorreDataProvider.defaultPlace
             )
     }
+
+    fun updateDetailsScreenStates(place: Place) {
+        _uiState.update {
+            it.copy(
+                currentSelectedPlace = place,
+                isShowingHomepage = false
+            )
+        }
+    }
+
+    fun resetHomeScreenStates() {
+        _uiState.update {
+            it.copy(
+                currentSelectedPlace = it.categories[it.currentCategory]?.get(0)
+                    ?: LocalLaTorreDataProvider.defaultPlace,
+                isShowingHomepage = true
+            )
+        }
+    }
+
+    fun updateCurrentCategory(category: PlaceCategory) {
+        _uiState.update {
+            it.copy(
+                currentCategory = category
+            )
+        }
+    }
+
 }
